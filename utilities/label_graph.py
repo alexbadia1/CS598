fbbaae3a729125a041a2d0c92fdb0688886c39d3
Adam <batesa@illinois.edu>
2024-09-19 10:29:27 -0500

    Updated graph labeling script

--- utilities/label_graph.py
+++ utilities/label_graph.py
@@ -1,16 +1,22 @@
 from graph_helpers import *
 
+# Graph Input File
 igraph_file = sys.argv[1]
 
+# Remove file extension for base filename
 base_fname = igraph_file
 if igraph_file.endswith("pkl.xz"):
     base_fname = igraph_file.replace(".pkl.xz", "")
 else:
     base_fname= igraph_file.replace(".pickle", "")
-    
-    
+
+# Labels Input File is csv of base fname
 seed_file = base_fname + ".csv"
-pdf_file = base_fname + ".pdf"
+
+# Output Files
+pdf_file = base_fname + ".pdf" # Graph vizualization
+label_file = base_fname + ".labels" # Full labels
+output_seeds_file = base_fname + ".seeds" # Seeds for reproducibility
 
 g = pickle_read(igraph_file)
 
@@ -27,10 +33,34 @@ for v in range(0,len(g.vs)):
     else:
         vertex["label"] = "benign"
 
+# Remove path from displayname
+display_name = base_fname.split("/")
+display_name = display_name[len(display_name)-1]
+
+# Write full label set out with ".labels" extension
+labels_f = open(label_file, "w")
 for v in range(0,len(g.vs)):
     vertex = g.vs[v]
-    if vertex["type"] == "process":
-        if "atlasv2" in base_fname:
-            print("%s, %d, %s, %s" % (base_fname, int(vertex["pid"]), vertex["name"], vertex["label"]))
-        else:
-            print("%s, %s, %s, %s" % (base_fname, vertex["uuid"], vertex["name"], vertex["label"]))
+    vertex_id = vertex["pid"] if "atlasv2" in base_fname else  vertex["uuid"]
+    labels_f.write("%s, %s, %s, %s\n"
+                   % (display_name, vertex_id, vertex["name"], vertex["label"]))
+labels_f.close()
+
+
+
+# Write root_cause/impact seed labels out with ".seeds" extension
+#  this is so that we can eventually rerun the labeling script with the whole graph in memory
+#  to ensure we capture all of the contaminates.
+seed_labels = get_seed_labels(seed_file)
+seeds_f = open(output_seeds_file, "w")
+
+g = pickle_read(igraph_file) # Pull fresh copy of graph to replicate dump_vertices format
+vertex_times(g)
+for v in range(0,len(g.vs)):
+    vertex = g.vs[v]
+    time = int(vertex["min_time"])
+    vertex_id = vertex["pid"] if "atlasv2" in base_fname else  vertex["uuid"]
+    if seed_labels[vertex_id] in ["root_cause", "impact"]:
+        seeds_f.write("%d,%s,\"%s\",%s,%s\n" %
+                      (time, vertex["type"], vertex["name"], vertex_id, seed_labels[vertex_id]))
+seeds_f.close()
